3d004ea8800645962ded579e645f951a933c7f38
LANDAU-SOFTWARE\jan.wiesemann <jwiesemann@landau-software.de>
2018-03-19 15:08:09 +0100

    Cleanup

--- WPF-Translate/WPF-Translate/ResourceDictionaryFile.cs
+++ WPF-Translate/WPF-Translate/ResourceDictionaryFile.cs
@@ -6,13 +6,13 @@ namespace de.LandauSoftware.WPFTranslate
     [DebuggerDisplay("{FileName} {Entrys.Count}")]
     public class ResourceDictionaryFile
     {
+        public const string MsCoreLibSystemNameSpace = "clr-namespace:System;assembly=mscorlib";
+
         public static readonly DictionaryNamespace[] DefaultNameSpaces = new DictionaryNamespace[]
-        {
+                {
             new DictionaryNamespace("", "http://schemas.microsoft.com/winfx/2006/xaml/presentation"),
             new DictionaryNamespace("x", "http://schemas.microsoft.com/winfx/2006/xaml")
         };
-
-        public const string MsCoreLibSystemNameSpace = "clr-namespace:System;assembly=mscorlib";
 
         private List<DictionaryNamespace> _AdditionalNamespaces = new List<DictionaryNamespace>();
         private List<RawDictionaryEntry> _Entrys = new List<RawDictionaryEntry>();
@@ -22,6 +22,14 @@ namespace de.LandauSoftware.WPFTranslate
         public ResourceDictionaryFile(string fileName)
         {
             _FileName = fileName;
+        }
+
+        public List<DictionaryNamespace> AdditionalNamespaces
+        {
+            get
+            {
+                return _AdditionalNamespaces;
+            }
         }
 
         public List<RawDictionaryEntry> Entrys
@@ -45,14 +53,6 @@ namespace de.LandauSoftware.WPFTranslate
             get
             {
                 return _MergedDictionarys;
-            }
-        }
-
-        public List<DictionaryNamespace> AdditionalNamespaces
-        {
-            get
-            {
-                return _AdditionalNamespaces;
             }
         }
     }
